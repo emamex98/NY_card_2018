09236df6069c12065b2ed2a688f0646b596218f2
Emanuel Estrada Larios <emamex98@users.noreply.github.com>
2017-12-27 01:42:50 -0600

    finished url gen

--- vendor/url-gen.js
+++ vendor/url-gen.js
@@ -3,6 +3,6 @@ $('#go').click(function() {
   var varName = $('#name').val();
   var varLang = $('#lang').val();
 
-  $('#url').val("url/" + varLang + ".html?name=" + varName);
+  $('#url').val("https://emamex98.github.io/NY_card_2018/" + varLang + ".html?name=" + varName);
 
 });
